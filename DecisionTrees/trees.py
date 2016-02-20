12337e8e5133fcf86bdba919498c7203fdf97c88
noprom <tyee.noprom@qq.com>
2016-02-20 17:08:03 +0800

    calcShannonEnt and create a simple data set

--- DecisionTrees/trees.py
+++ DecisionTrees/trees.py
@@ -8,16 +8,24 @@ Created on 2/20/16
 
 from math import log
 
-def calcShanonEnt(dataSet):
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing','flippers']
+    return dataSet, labels
+
+def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     labelCounts = {}
-    for fectVec in dataSet:
-        currentLabel = fectVec[-1]
-        if currentLabel not in labelCounts.keys():
-            labelCounts[currentLabel] = 0
-            labelCounts[currentLabel] += 1
+    for featVec in dataSet: #the the number of unique elements and their occurance
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
     shannonEnt = 0.0
     for key in labelCounts:
         prob = float(labelCounts[key])/numEntries
-        shannonEnt -= prob * log(prob, 2)
+        shannonEnt -= prob * log(prob,2) #log base 2
     return shannonEnt
