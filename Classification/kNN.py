13471b8c3060318353c497acee2d2c117f7cfe98
noprom <tyee.noprom@qq.com>
2016-02-20 14:49:45 +0800

    classify the person

--- Classification/kNN.py
+++ Classification/kNN.py
@@ -82,3 +82,13 @@ def datingClassTest():
     print "the total error rate is: %f" % (errorCount / float(numTestVecs))
     print errorCount
 
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(raw_input("percentage of time spent playing video games?"))
+    ffMiles = float(raw_input("frequent flier miles earned per year?"))
+    iceCream = float(raw_input("liters of ice cream consumes per year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr - minVals)/ranges, normMat, datingLabels, 3)
+    print "You will probably like this person:", resultList[classifierResult - 1]
