400a7cd9d9cf7116ecc2c1d2ed2727af9d89040e
noprom <tyee.noprom@qq.com>
2016-02-20 20:47:18 +0800

    greatestCnt

--- DecisionTrees/trees.py
+++ DecisionTrees/trees.py
@@ -7,6 +7,7 @@ Created on 2/20/16
 '''
 
 from math import log
+import operator
 
 def createDataSet():
     dataSet = [[1, 1, 'yes'],
@@ -38,3 +39,32 @@ def splitDataSet(dataSet, axis, value):
             reducedFectVec.extend(fectVec[axis + 1:])
             retDataSet.append(reducedFectVec)
     return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1      #the last column is used for the labels
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0; bestFeature = -1
+    for i in range(numFeatures):        #iterate over all the features
+        featList = [example[i] for example in dataSet]#create a list of all the examples of this feature
+        uniqueVals = set(featList)       #get a set of unique values
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy     #calculate the info gain; ie reduction in entropy
+        if (infoGain > bestInfoGain):       #compare this to the best gain so far
+            bestInfoGain = infoGain         #if better than current best, set to best
+            bestFeature = i
+    return bestFeature                      #returns an integer
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reversed=True)
+    return sortedClassCount[0][0]
+
+
+
